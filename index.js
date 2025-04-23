decbbdf3f1dab1ea1c883eb8cf0d9ca087d98c47
guanguer <jftabord@gmail.com>
2025-04-22 22:06:55 -0700

    add database

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const path = require('path')
 const cool = require('cool-ascii-faces')
+const { Pool } = require('pg')
 
 const port = process.env.PORT || 5006
 
 const app = express()
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
@@ -28,6 +36,21 @@ app.get('/times', (req, res) => {
         result += i + ' '
     }
     res.send(result)
+})
+
+app.get('/db', async(req, res) => {
+  console.log(`Rendering the results of a database query for route '/db'`)
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table')
+    res.render('pages/db', {
+      results: result ? result.rows : null
+    })
+    client.release()
+  } catch (err) {
+    console.error(err)
+    res.send(`Error ${err}`)
+  }
 })
 
 const server = app.listen(port, () => {
